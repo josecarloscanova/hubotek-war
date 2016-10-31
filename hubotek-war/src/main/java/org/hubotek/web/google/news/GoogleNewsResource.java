90b260a602c6c2f67c18e3261da8a0c73dd8848c
Darth Vader. <jose.carlos.canova@gmail.com>
2016-10-31 00:54:40 -0300

    added search qs

--- hubotek-war/src/main/java/org/hubotek/web/google/news/GoogleNewsResource.java
+++ hubotek-war/src/main/java/org/hubotek/web/google/news/GoogleNewsResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.StreamingOutput;
@@ -72,4 +73,16 @@ public class GoogleNewsResource {
 		};
 	}
 
+	@GET
+	@Path("/search")
+	@Produces(MediaType.APPLICATION_XML)
+	public StreamingOutput searc(@QueryParam("s") String search)
+	{ 
+		return new StreamingOutput() {
+			public void write(OutputStream outputStream)
+					throws IOException, WebApplicationException {
+				outputStream.write(googleNewsService.processRequestSearch(search).getBytes());
+			}
+		};
+	}
 }
